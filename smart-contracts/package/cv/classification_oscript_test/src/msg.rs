189f94e94b7ba97b90cd9418d7be2dd06bd5938c
Le Duc Pham <ducphamle212@gmail.com>
2021-03-18 16:43:29 +0700

    add nl033,cv023+4,classification oscript query api

---
+++ smart-contracts/package/cv/classification_oscript_test/src/msg.rs
@@ -0,0 +1,38 @@
+use cosmwasm_std::CustomQuery;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InitMsg {
+    pub ai_data_source: Vec<String>,
+    pub testcase: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum HandleMsg {
+    UpdateDatasource { name: Vec<String> },
+    UpdateTestcase { name: Vec<String> },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetDatasource {},
+    GetTestcase {},
+    Aggregate { results: Vec<String> },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+/// An implementation of QueryRequest::Custom to show this works and can be extended in the contract
+pub enum SpecialQuery {
+    Fetch {
+        url: String,
+        body: String,
+        method: String,
+        authorization: String,
+    },
+}
+impl CustomQuery for SpecialQuery {}
