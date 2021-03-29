1a83fcf057162be49f955646cf3a288fd52040d5
tuorai <tubackkhoa@gmail.com>
2021-03-29 14:45:29 +0700

    add all left cv services

---
+++ smart-contracts/package/cv/cv013/src/contract.rs
@@ -0,0 +1,55 @@
+use crate::error::ContractError;
+use crate::msg::{HandleMsg, InitMsg, QueryMsg, SpecialQuery};
+use cosmwasm_std::{
+    Api, Binary, Env, Extern, HandleResponse, InitResponse, MessageInfo, Querier, StdResult,
+    Storage,
+};
+
+// Note, you can use StdResult in some functions where you do not
+// make use of the custom errors
+pub fn init<S: Storage, A: Api, Q: Querier>(
+    _deps: &mut Extern<S, A, Q>,
+    _env: Env,
+    _info: MessageInfo,
+    _: InitMsg,
+) -> StdResult<InitResponse> {
+    Ok(InitResponse::default())
+}
+
+// And declare a custom Error variant for the ones where you will want to make use of it
+pub fn handle<S: Storage, A: Api, Q: Querier>(
+    _: &mut Extern<S, A, Q>,
+    _env: Env,
+    _: MessageInfo,
+    _: HandleMsg,
+) -> Result<HandleResponse, ContractError> {
+    Ok(HandleResponse::default())
+}
+
+pub fn query<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    _env: Env,
+    msg: QueryMsg,
+) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::Get { input } => query_data(deps, input),
+    }
+}
+
+fn query_data<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    input: String,
+) -> StdResult<Binary> {
+    let req = SpecialQuery::Fetch {
+        url: "https://100api.orai.dev/cv013".to_string(),
+        // body is in url-encoded format
+        body: input,
+        method: "POST".to_string(),
+        authorization: "".to_string(),
+    }
+    .into();
+
+    // return binary so that blockchain return JSON.stringify of the result
+    let data: Binary = deps.querier.custom_query(&req)?;
+    Ok(data)
+}
