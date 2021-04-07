672b497de8101a20ee11d9e033f3897cfe8d07c6
tuorai <tubackkhoa@gmail.com>
2021-04-07 17:40:25 +0700

    no need to export state

--- smart-contracts/package/sample/aioracle/examples/schema.rs
+++ smart-contracts/package/sample/aioracle/examples/schema.rs
@@ -4,7 +4,6 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
 use aioracle::msg::{HandleMsg, InitMsg, QueryMsg};
-use aioracle::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -18,5 +17,4 @@ fn main() {
     export_schema(&schema_for!(InitMsg), &out_dir);
     export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
 }
