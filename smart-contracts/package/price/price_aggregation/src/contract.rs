40781ad52928c9ef86943f3c05fce45f169024a1
Le Duc Pham <ducphamle212@gmail.com>
2021-05-14 18:22:57 +0700

    merge files

---
+++ smart-contracts/package/price/price_aggregation/src/contract.rs
@@ -0,0 +1,54 @@
+use std::fmt::format;
+
+use crate::error::ContractError;
+use crate::msg::{HandleMsg, InitMsg, Input, QueryMsg, SpecialQuery};
+use cosmwasm_std::{
+    from_slice, to_binary, Api, Binary, Env, Extern, HandleResponse, InitResponse, MessageInfo,
+    Querier, StdResult, Storage,
+};
+
+// Note, you can use StdResult in some functions where you do not
+// make use of the custom errors
+pub fn init<S: Storage, A: Api, Q: Querier>(
+    _deps: &mut Extern<S, A, Q>,
+    _env: Env,
+    _info: MessageInfo,
+    _: InitMsg,
+) -> StdResult<InitResponse> {
+    Ok(InitResponse::default())
+}
+
+// And declare a custom Error variant for the ones where you will want to make use of it
+pub fn handle<S: Storage, A: Api, Q: Querier>(
+    _: &mut Extern<S, A, Q>,
+    _env: Env,
+    _: MessageInfo,
+    _: HandleMsg,
+) -> Result<HandleResponse, ContractError> {
+    Ok(HandleResponse::default())
+}
+
+pub fn query<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    _env: Env,
+    msg: QueryMsg,
+) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::Get { input } => query_data(deps, input),
+    }
+}
+
+fn query_data<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    input: String,
+) -> StdResult<Binary> {
+    let req = SpecialQuery::Fetch {
+        url: format!("http://lcd.orai.io/airesult/fullreq/{}", input),
+        body: String::from(""),
+        method: "GET".to_string(),
+        authorization: "".to_string(),
+    }
+    .into();
+    let response: Binary = deps.querier.custom_query(&req)?;
+    Ok(response)
+}
