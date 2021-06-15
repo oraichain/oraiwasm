9b28b0fa3e33313b0fd8cad8e618733324140b90
Le Duc Pham <ducphamle212@gmail.com>
2021-06-15 04:12:11 +0000

    separate dsource price into diff contracts

---
+++ package/price/dsource_gate/src/msg.rs
@@ -0,0 +1,90 @@
+use cosmwasm_std::CustomQuery;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InitMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Data {
+    pub name: String,
+    pub prices: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Binance {
+    pub symbol: String,
+    pub price: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Coinbase {
+    pub data: CoinbaseData,
+}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CoinbaseData {
+    pub base: String,
+    pub currency: String,
+    pub amount: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Gate {
+    pub currency_pair: String,
+    pub last: String,
+    pub lowest_ask: String,
+    pub highest_bid: String,
+    pub change_percentage: String,
+    pub base_volume: String,
+    pub quote_volume: String,
+    pub high_24h: String,
+    pub low_24h: String,
+}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CryptoCompare {
+    pub USD: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CoinCap {
+    pub data: CoinCapData,
+    pub timestamp: u128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CoinCapData {
+    pub id: String,
+    pub rank: String,
+    pub symbol: String,
+    pub name: String,
+    pub suppy: String,
+    pub maxSupply: String,
+    pub marketCapUsd: String,
+    pub volumeUsd24Hr: String,
+    pub priceUsd: String,
+    pub changePercent24Hr: String,
+    pub vwap24Hr: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum HandleMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    Get { input: String },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+/// An implementation of QueryRequest::Custom to show this works and can be extended in the contract
+pub enum SpecialQuery {
+    Fetch {
+        url: String,
+        body: String,
+        method: String,
+        authorization: String,
+    },
+}
+impl CustomQuery for SpecialQuery {}
