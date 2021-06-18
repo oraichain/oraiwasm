41de3ae296438db42f3f046580179baf4d872ff8
tuorai <tubackkhoa@gmail.com>
2021-06-18 08:32:48 +0700

    remove redundant folder

--- smart-contracts/package/ether/erc20/src/state.rs
+++
@@ -1,9 +0,0 @@
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Debug, Deserialize, Clone, PartialEq, JsonSchema)]
-pub struct Constants {
-    pub name: String,
-    pub symbol: String,
-    pub decimals: u8,
-}
