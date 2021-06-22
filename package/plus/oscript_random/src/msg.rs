a35bec0a2dd93d3c9bf16301e9a7faa2629c9885
Le Duc Pham <ducphamle212@gmail.com>
2021-06-22 15:06:17 +0000

    add random service

---
+++ package/plus/oscript_random/src/msg.rs
@@ -0,0 +1,36 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InitMsg {
+    pub ai_data_source: Vec<String>,
+    pub testcase: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct Input {
+    pub random: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct Output {
+    pub random: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum HandleMsg {
+    UpdateDatasource { name: Vec<String> },
+    UpdateTestcase { name: Vec<String> },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetDatasource {},
+    GetTestcase {},
+    Aggregate { results: Vec<String> },
+}
