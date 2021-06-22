d6f66549f21cef414e2d3a83ed305dff2eae6e1c
tuorai <tubackkhoa@gmail.com>
2021-06-22 19:14:33 +0700

    add doc

--- package/price/dsource_coingecko/examples/schema.rs
+++ package/price/dsource_coingecko/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use datasource_eth::msg::{HandleMsg, InitMsg, QueryMsg, SpecialQuery};
+use dsource_coingecko::msg::{HandleMsg, InitMsg, QueryMsg, SpecialQuery};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
