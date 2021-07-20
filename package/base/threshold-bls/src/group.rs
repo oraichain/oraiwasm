2f44c89a2dcc07aeb93ea024513542d1e7377d35
tuorai <tubackkhoa@gmail.com>
2021-07-20 04:12:06 +0000

    add threshold-bls

---
+++ package/base/threshold-bls/src/group.rs
@@ -0,0 +1,108 @@
+//! Traits for operating on Groups and Elliptic Curves.
+
+use rand_core::RngCore;
+use serde::{Deserialize, Serialize};
+use std::fmt::{Debug, Display};
+use std::marker::PhantomData;
+
+/// Element represents an element of a group with the additive notation
+/// which is also equipped with a multiplication transformation.
+/// Two implementations are for Scalar which forms a ring so RHS is the same
+/// and Point which can be multiplied by a scalar of its prime field.
+pub trait Element:
+    Clone + Display + Debug + Eq + Serialize + for<'a> Deserialize<'a> + PartialEq + Send + Sync
+{
+    /// The right-hand-side argument for multiplication
+    type RHS;
+
+    /// Returns the zero element of the group
+    fn new() -> Self;
+
+    /// Returns the one element of the group
+    fn one() -> Self;
+
+    /// Adds the RHS  element to the LHS element in place
+    fn add(&mut self, s2: &Self);
+
+    /// Multiplies the LHS element by the RHS element in place
+    fn mul(&mut self, mul: &Self::RHS);
+
+    /// Samples a random element using the provided RNG
+    fn rand<R: RngCore>(rng: &mut R) -> Self;
+
+    /// Returns the zero element of the group
+    fn zero() -> Self {
+        Self::new()
+    }
+}
+
+/// Scalar can be multiplied by only a Scalar, no other elements.
+pub trait Scalar: Element {
+    fn set_int(&mut self, i: u64);
+    fn inverse(&self) -> Option<Self>;
+    fn negate(&mut self);
+    fn sub(&mut self, other: &Self);
+    // TODO
+}
+
+/// Basic point functionality that can be multiplied by a scalar
+pub trait Point: Element {
+    /// Error which may occur while mapping to the group
+    type Error: Debug;
+
+    /// Maps the provided data to a group element
+    fn map(&mut self, data: &[u8]) -> Result<(), <Self as Point>::Error>;
+}
+
+/// A group holds functionalities to create scalar and points related; it is
+/// similar to the Engine definition, just much more simpler.
+pub trait Curve: Clone + Debug + Send + Sync {
+    /// The curve's scalar
+    type Scalar: Scalar<RHS = Self::Scalar>;
+
+    /// The curve's point
+    type Point: Point<RHS = Self::Scalar>;
+
+    /// scalar returns the identity element of the field.
+    fn scalar() -> Self::Scalar {
+        Self::Scalar::new()
+    }
+
+    /// point returns the default additive generator of the group.
+    fn point() -> Self::Point {
+        Self::Point::one()
+    }
+}
+
+/// A curve equipped with a bilinear pairing operation.
+pub trait PairingCurve: Debug {
+    type Scalar: Scalar<RHS = Self::Scalar>;
+
+    type G1: Point<RHS = Self::Scalar>;
+
+    type G2: Point<RHS = Self::Scalar>;
+
+    type GT: Element;
+
+    /// Perfors a pairing operation between the 2 group elements
+    fn pair(a: &Self::G1, b: &Self::G2) -> Self::GT;
+}
+
+#[derive(Debug, Clone, PartialEq)]
+/// Helper which binds together a scalar with a group type to form a curve
+pub struct CurveFrom<S: Scalar, P: Point> {
+    s: PhantomData<S>,
+    p: PhantomData<P>,
+}
+
+impl<S, P> Curve for CurveFrom<S, P>
+where
+    S: Scalar<RHS = S>,
+    P: Point<RHS = S>,
+{
+    type Scalar = S;
+    type Point = P;
+}
+
+pub(super) type G1Curve<C> = CurveFrom<<C as PairingCurve>::Scalar, <C as PairingCurve>::G1>;
+pub(super) type G2Curve<C> = CurveFrom<<C as PairingCurve>::Scalar, <C as PairingCurve>::G2>;
---
+++ package/base/threshold-bls/src/sig/bls.rs
@@ -0,0 +1,188 @@
+use crate::group::{Element, PairingCurve, Point};
+use crate::sig::{Scheme, SignatureScheme};
+use std::{fmt::Debug, marker::PhantomData};
+use thiserror::Error;
+
+/// BLSError are thrown out when using the BLS signature scheme.
+#[derive(Debug, Error)]
+pub enum BLSError {
+    /// InvalidSig is raised when the validation routine of the BLS algorithm
+    /// does not finish successfully,i.e. it is an invalid signature.
+    #[error("invalid signature")]
+    InvalidSig,
+
+    #[error("could not hash to curve")]
+    HashingError,
+
+    #[error("could not deserialize: {0}")]
+    DeserializationError(#[from] bincode::Error),
+}
+
+// private module workaround to avoid leaking a private
+// trait into a public trait
+// see https://github.com/rust-lang/rust/issues/34537
+// XXX another way to pull it off without this hack?
+pub mod common {
+    use super::*;
+
+    /// BLSScheme is an internal trait that encompasses the common work between a
+    /// BLS signature over G1 or G2.
+    pub trait BLSScheme: Scheme {
+        /// Returns sig = msg^{private}. The message MUST be hashed before this call.
+        fn internal_sign(
+            private: &Self::Private,
+            msg: &[u8],
+            should_hash: bool,
+        ) -> Result<Vec<u8>, BLSError> {
+            let mut h = if should_hash {
+                let mut h = Self::Signature::new();
+                h.map(msg).map_err(|_| BLSError::HashingError)?;
+                h
+            } else {
+                bincode::deserialize_from(msg)?
+            };
+
+            h.mul(private);
+
+            let serialized = bincode::serialize(&h)?;
+            Ok(serialized)
+        }
+
+        fn internal_verify(
+            public: &Self::Public,
+            msg: &[u8],
+            sig_bytes: &[u8],
+            should_hash: bool,
+        ) -> Result<(), BLSError> {
+            let sig: Self::Signature = bincode::deserialize_from(sig_bytes)?;
+
+            let h = if should_hash {
+                let mut h = Self::Signature::new();
+                h.map(msg).map_err(|_| BLSError::HashingError)?;
+                h
+            } else {
+                bincode::deserialize_from(msg)?
+            };
+
+            let success = Self::final_exp(public, &sig, &h);
+            if !success {
+                return Err(BLSError::InvalidSig);
+            }
+
+            Ok(())
+        }
+
+        /// Performs the final exponentiation for the BLS sig scheme
+        fn final_exp(p: &Self::Public, sig: &Self::Signature, hm: &Self::Signature) -> bool;
+    }
+
+    impl<T> SignatureScheme for T
+    where
+        T: BLSScheme,
+    {
+        type Error = BLSError;
+
+        fn sign(private: &Self::Private, msg: &[u8]) -> Result<Vec<u8>, Self::Error> {
+            T::internal_sign(private, msg, true)
+        }
+
+        /// Verifies the signature by the provided public key
+        fn verify(
+            public: &Self::Public,
+            msg_bytes: &[u8],
+            sig_bytes: &[u8],
+        ) -> Result<(), Self::Error> {
+            T::internal_verify(public, msg_bytes, sig_bytes, true)
+        }
+    }
+}
+
+/// G1Scheme implements the BLS signature scheme with G1 as private / public
+/// keys and G2 as signature elements over the given pairing curve.
+#[derive(Clone, Debug)]
+pub struct G1Scheme<C: PairingCurve> {
+    m: PhantomData<C>,
+}
+
+impl<C> Scheme for G1Scheme<C>
+where
+    C: PairingCurve,
+{
+    type Private = C::Scalar;
+    type Public = C::G1;
+    type Signature = C::G2;
+}
+
+impl<C> common::BLSScheme for G1Scheme<C>
+where
+    C: PairingCurve,
+{
+    fn final_exp(p: &Self::Public, sig: &Self::Signature, hm: &Self::Signature) -> bool {
+        // e(g1,sig) == e(pub, H(m))
+        // e(g1,H(m))^x == e(g1,H(m))^x
+        let left = C::pair(&C::G1::one(), &sig);
+        let right = C::pair(p, &hm);
+        left == right
+    }
+}
+
+/// G2Scheme implements the BLS signature scheme with G2 as private / public
+/// keys and G1 as signature elements over the given pairing curve.
+#[derive(Clone, Debug)]
+pub struct G2Scheme<C: PairingCurve> {
+    m: PhantomData<C>,
+}
+
+impl<C> Scheme for G2Scheme<C>
+where
+    C: PairingCurve,
+{
+    type Private = C::Scalar;
+    type Public = C::G2;
+    type Signature = C::G1;
+}
+
+impl<C> common::BLSScheme for G2Scheme<C>
+where
+    C: PairingCurve,
+{
+    fn final_exp(p: &Self::Public, sig: &Self::Signature, hm: &Self::Signature) -> bool {
+        // e(sig,g2) == e(H(m),pub)
+        // e(H(m),g2)^x == e(H(m),g2)^x
+        let left = C::pair(&sig, &Self::Public::one());
+        let right = C::pair(&hm, p);
+        left == right
+    }
+}
+
+#[cfg(feature = "bls12_381")]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::curve::bls12381::{Curve as G1Curve, G2Curve, PairingCurve as PCurve};
+    use crate::group::Curve;
+    use rand::prelude::*;
+
+    fn keypair<C: Curve>() -> (C::Scalar, C::Point) {
+        let private = C::Scalar::rand(&mut thread_rng());
+        let mut public = C::Point::one();
+        public.mul(&private);
+        (private, public)
+    }
+
+    #[test]
+    fn nbls_g2() {
+        let (private, public) = keypair::<G2Curve>();
+        let msg = vec![1, 9, 6, 9];
+        let sig = G2Scheme::<PCurve>::sign(&private, &msg).unwrap();
+        G2Scheme::<PCurve>::verify(&public, &msg, &sig).expect("that should not happen");
+    }
+
+    #[test]
+    fn nbls_g1() {
+        let (private, public) = keypair::<G1Curve>();
+        let msg = vec![1, 9, 6, 9];
+        let sig = G1Scheme::<PCurve>::sign(&private, &msg).unwrap();
+        G1Scheme::<PCurve>::verify(&public, &msg, &sig).expect("that should not happen");
+    }
+}
