ec8fb04846ec148fa675bf559ad572b86c526d23
tuorai <tubackkhoa@gmail.com>
2021-09-07 15:36:01 +0700

    add hub market with permissions

---
+++ package/plus/hub_market/src/contract.rs
@@ -0,0 +1,318 @@
+use schemars::JsonSchema;
+use std::fmt;
+
+use cosmwasm_std::{
+    attr, to_binary, Api, Binary, CanonicalAddr, CosmosMsg, Deps, DepsMut, Empty, Env,
+    HandleResponse, HumanAddr, InitResponse, MessageInfo, StdResult,
+};
+
+use crate::error::ContractError;
+use crate::msg::{AdminListResponse, CanExecuteResponse, HandleMsg, InitMsg, QueryMsg};
+use crate::state::{admin_list, admin_list_read, AdminList};
+
+pub fn init(deps: DepsMut, _env: Env, info: MessageInfo, msg: InitMsg) -> StdResult<InitResponse> {
+    // list of whitelist
+    let cfg = AdminList {
+        admins: map_canonical(deps.api, &msg.admins)?,
+        mutable: msg.mutable,
+        owner: deps.api.canonical_address(&info.sender)?,
+    };
+    admin_list(deps.storage).save(&cfg)?;
+    Ok(InitResponse::default())
+}
+
+fn map_canonical(api: &dyn Api, admins: &[HumanAddr]) -> StdResult<Vec<CanonicalAddr>> {
+    admins
+        .iter()
+        .map(|addr| api.canonical_address(addr))
+        .collect()
+}
+
+fn map_human(api: &dyn Api, admins: &[CanonicalAddr]) -> StdResult<Vec<HumanAddr>> {
+    admins.iter().map(|addr| api.human_address(addr)).collect()
+}
+
+pub fn handle(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    // Note: implement this function with different type to add support for custom messages
+    // and then import the rest of this contract code.
+    msg: HandleMsg<Empty>,
+) -> Result<HandleResponse<Empty>, ContractError> {
+    match msg {
+        HandleMsg::Execute { msgs } => handle_execute(deps, env, info, msgs),
+        HandleMsg::Freeze {} => handle_freeze(deps, env, info),
+        HandleMsg::UpdateAdmins { admins } => handle_update_admins(deps, env, info, admins),
+    }
+}
+
+pub fn handle_execute<T>(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msgs: Vec<CosmosMsg<T>>,
+) -> Result<HandleResponse<T>, ContractError>
+where
+    T: Clone + fmt::Debug + PartialEq + JsonSchema,
+{
+    if !can_execute(deps.as_ref(), &info.sender)? {
+        Err(ContractError::Unauthorized {})
+    } else {
+        // switch/case for storage and implementation
+        let mut res = HandleResponse::default();
+        res.messages = msgs;
+        res.attributes = vec![attr("action", "execute")];
+        Ok(res)
+    }
+}
+
+pub fn handle_freeze(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+) -> Result<HandleResponse, ContractError> {
+    let mut cfg = admin_list_read(deps.storage).load()?;
+    if !cfg.can_modify(&deps.api.canonical_address(&info.sender)?) {
+        Err(ContractError::Unauthorized {})
+    } else {
+        cfg.mutable = false;
+        admin_list(deps.storage).save(&cfg)?;
+
+        let mut res = HandleResponse::default();
+        res.attributes = vec![attr("action", "freeze")];
+        Ok(res)
+    }
+}
+
+pub fn handle_update_admins(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    admins: Vec<HumanAddr>,
+) -> Result<HandleResponse, ContractError> {
+    let mut cfg = admin_list_read(deps.storage).load()?;
+    if !cfg.can_modify(&deps.api.canonical_address(&info.sender)?) {
+        Err(ContractError::Unauthorized {})
+    } else {
+        cfg.admins = map_canonical(deps.api, &admins)?;
+        admin_list(deps.storage).save(&cfg)?;
+
+        let mut res = HandleResponse::default();
+        res.attributes = vec![attr("action", "update_admins")];
+        Ok(res)
+    }
+}
+
+fn can_execute(deps: Deps, sender: &HumanAddr) -> StdResult<bool> {
+    let cfg = admin_list_read(deps.storage).load()?;
+    let can = cfg.is_admin(&deps.api.canonical_address(sender)?);
+    Ok(can)
+}
+
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::AdminList {} => to_binary(&query_admin_list(deps)?),
+        QueryMsg::CanExecute { sender, msg } => to_binary(&query_can_execute(deps, sender, msg)?),
+    }
+}
+
+pub fn query_admin_list(deps: Deps) -> StdResult<AdminListResponse> {
+    let cfg = admin_list_read(deps.storage).load()?;
+    Ok(AdminListResponse {
+        admins: map_human(deps.api, &cfg.admins)?,
+        mutable: cfg.mutable,
+    })
+}
+
+pub fn query_can_execute(
+    deps: Deps,
+    sender: HumanAddr,
+    _msg: CosmosMsg,
+) -> StdResult<CanExecuteResponse> {
+    Ok(CanExecuteResponse {
+        can_execute: can_execute(deps, &sender)?,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info, MOCK_CONTRACT_ADDR};
+    use cosmwasm_std::{coin, coins, BankMsg, StakingMsg, WasmMsg};
+
+    #[test]
+    fn init_and_modify_config() {
+        let mut deps = mock_dependencies(&[]);
+
+        let alice = HumanAddr::from("alice");
+        let bob = HumanAddr::from("bob");
+        let carl = HumanAddr::from("carl");
+        let owner = HumanAddr::from("tupt");
+        let anyone = HumanAddr::from("anyone");
+
+        // init the contract
+        let init_msg = InitMsg {
+            admins: vec![alice.clone(), bob.clone(), carl.clone()],
+            mutable: true,
+        };
+        let info = mock_info(&owner, &[]);
+        init(deps.as_mut(), mock_env(), info, init_msg).unwrap();
+
+        // ensure expected config
+        let expected = AdminListResponse {
+            admins: vec![alice.clone(), bob.clone(), carl.clone()],
+            mutable: true,
+        };
+        assert_eq!(query_admin_list(deps.as_ref()).unwrap(), expected);
+
+        // anyone cannot modify the contract
+        let msg = HandleMsg::UpdateAdmins {
+            admins: vec![anyone.clone()],
+        };
+        let info = mock_info(&anyone, &[]);
+        let res = handle(deps.as_mut(), mock_env(), info, msg);
+        match res.unwrap_err() {
+            ContractError::Unauthorized { .. } => {}
+            e => panic!("unexpected error: {}", e),
+        }
+
+        // but alice can kick out carl
+        let msg = HandleMsg::UpdateAdmins {
+            admins: vec![alice.clone(), bob.clone()],
+        };
+        let info = mock_info(&alice, &[]);
+        handle(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+        // ensure expected config
+        let expected = AdminListResponse {
+            admins: vec![alice.clone(), bob.clone()],
+            mutable: true,
+        };
+        assert_eq!(query_admin_list(deps.as_ref()).unwrap(), expected);
+
+        // carl cannot freeze it
+        let info = mock_info(&carl, &[]);
+        let res = handle(deps.as_mut(), mock_env(), info, HandleMsg::Freeze {});
+        match res.unwrap_err() {
+            ContractError::Unauthorized { .. } => {}
+            e => panic!("unexpected error: {}", e),
+        }
+
+        // but bob can
+        let info = mock_info(&bob, &[]);
+        handle(deps.as_mut(), mock_env(), info, HandleMsg::Freeze {}).unwrap();
+        let expected = AdminListResponse {
+            admins: vec![alice.clone(), bob.clone()],
+            mutable: false,
+        };
+        assert_eq!(query_admin_list(deps.as_ref()).unwrap(), expected);
+
+        // and now alice cannot change it again
+        let msg = HandleMsg::UpdateAdmins {
+            admins: vec![alice.clone()],
+        };
+        let info = mock_info(&alice, &[]);
+        let res = handle(deps.as_mut(), mock_env(), info, msg);
+        match res.unwrap_err() {
+            ContractError::Unauthorized { .. } => {}
+            e => panic!("unexpected error: {}", e),
+        }
+    }
+
+    #[test]
+    fn execute_messages_has_proper_permissions() {
+        let mut deps = mock_dependencies(&[]);
+
+        let alice = HumanAddr::from("alice");
+        let bob = HumanAddr::from("bob");
+        let carl = HumanAddr::from("carl");
+        let owner = HumanAddr::from("tupt");
+
+        // init the contract
+        let init_msg = InitMsg {
+            admins: vec![alice.clone(), carl.clone()],
+            mutable: false,
+        };
+        let info = mock_info(&owner, &[]);
+        init(deps.as_mut(), mock_env(), info, init_msg).unwrap();
+
+        let freeze: HandleMsg<Empty> = HandleMsg::Freeze {};
+        let msgs = vec![
+            BankMsg::Send {
+                from_address: HumanAddr::from(MOCK_CONTRACT_ADDR),
+                to_address: bob.clone(),
+                amount: coins(10000, "DAI"),
+            }
+            .into(),
+            WasmMsg::Execute {
+                contract_addr: HumanAddr::from("some contract"),
+                msg: to_binary(&freeze).unwrap(),
+                send: vec![],
+            }
+            .into(),
+        ];
+
+        // make some nice message
+        let handle_msg = HandleMsg::Execute { msgs: msgs.clone() };
+
+        // bob cannot execute them
+        let info = mock_info(&bob, &[]);
+        let res = handle(deps.as_mut(), mock_env(), info, handle_msg.clone());
+        match res.unwrap_err() {
+            ContractError::Unauthorized { .. } => {}
+            e => panic!("unexpected error: {}", e),
+        }
+
+        // but carl can
+        let info = mock_info(&carl, &[]);
+        let res = handle(deps.as_mut(), mock_env(), info, handle_msg.clone()).unwrap();
+        assert_eq!(res.messages, msgs);
+        assert_eq!(res.attributes, vec![attr("action", "execute")]);
+    }
+
+    #[test]
+    fn can_execute_query_works() {
+        let mut deps = mock_dependencies(&[]);
+
+        let alice = HumanAddr::from("alice");
+        let bob = HumanAddr::from("bob");
+        let owner = HumanAddr::from("tupt");
+        let anyone = HumanAddr::from("anyone");
+
+        // init the contract
+        let init_msg = InitMsg {
+            admins: vec![alice.clone(), bob.clone()],
+            mutable: false,
+        };
+        let info = mock_info(&owner, &[]);
+        init(deps.as_mut(), mock_env(), info, init_msg).unwrap();
+
+        // let us make some queries... different msg types by owner and by other
+        let send_msg = CosmosMsg::Bank(BankMsg::Send {
+            from_address: MOCK_CONTRACT_ADDR.into(),
+            to_address: anyone.clone(),
+            amount: coins(12345, "ushell"),
+        });
+        let staking_msg = CosmosMsg::Staking(StakingMsg::Delegate {
+            validator: anyone.clone(),
+            amount: coin(70000, "ureef"),
+        });
+
+        // owner can send
+        let res = query_can_execute(deps.as_ref(), alice.clone(), send_msg.clone()).unwrap();
+        assert_eq!(res.can_execute, true);
+
+        // owner can stake
+        let res = query_can_execute(deps.as_ref(), bob.clone(), staking_msg.clone()).unwrap();
+        assert_eq!(res.can_execute, true);
+
+        // anyone cannot send
+        let res = query_can_execute(deps.as_ref(), anyone.clone(), send_msg.clone()).unwrap();
+        assert_eq!(res.can_execute, false);
+
+        // anyone cannot stake
+        let res = query_can_execute(deps.as_ref(), anyone.clone(), staking_msg.clone()).unwrap();
+        assert_eq!(res.can_execute, false);
+    }
+}
---
+++ package/plus/hub_market/src/state.rs
@@ -0,0 +1,95 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::{CanonicalAddr, Storage};
+use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
+pub struct AdminList {
+    pub admins: Vec<CanonicalAddr>,
+    pub owner: CanonicalAddr,
+    pub mutable: bool,
+}
+
+impl AdminList {
+    /// returns true if the address is a registered admin
+    pub fn is_admin(&self, addr: &CanonicalAddr) -> bool {
+        // owner is admin
+        if self.owner.eq(addr) {
+            return true;
+        }
+        self.admins.iter().any(|a| a == addr)
+    }
+
+    /// returns true if the address is a registered admin and the config is mutable
+    pub fn can_modify(&self, addr: &CanonicalAddr) -> bool {
+        if self.owner.eq(addr) {
+            return true;
+        }
+        self.mutable && self.admins.iter().any(|a| a == addr)
+    }
+}
+
+pub const ADMIN_LIST_KEY: &[u8] = b"admin_list";
+
+// config is all config information
+pub fn admin_list(storage: &mut dyn Storage) -> Singleton<AdminList> {
+    singleton(storage, ADMIN_LIST_KEY)
+}
+
+pub fn admin_list_read(storage: &dyn Storage) -> ReadonlySingleton<AdminList> {
+    singleton_read(storage, ADMIN_LIST_KEY)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::MockApi;
+    use cosmwasm_std::{Api, HumanAddr};
+
+    #[test]
+    fn is_admin() {
+        let api = MockApi::default();
+        let admins: Vec<_> = vec!["bob", "paul", "john"]
+            .into_iter()
+            .map(|name| api.canonical_address(&HumanAddr::from(name)).unwrap())
+            .collect();
+        let owner = api.canonical_address(&"tupt".into()).unwrap();
+        let config = AdminList {
+            admins: admins.clone(),
+            mutable: false,
+            owner: owner.clone(),
+        };
+        assert!(config.is_admin(&owner));
+        assert!(config.is_admin(&admins[0]));
+        assert!(config.is_admin(&admins[2]));
+        let other = api.canonical_address(&HumanAddr::from("other")).unwrap();
+        assert!(!config.is_admin(&other));
+    }
+
+    #[test]
+    fn can_modify() {
+        let api = MockApi::default();
+        let alice = api.canonical_address(&HumanAddr::from("alice")).unwrap();
+        let bob = api.canonical_address(&HumanAddr::from("bob")).unwrap();
+        let owner = api.canonical_address(&HumanAddr::from("tupt")).unwrap();
+
+        // admin can modify mutable contract
+        let config = AdminList {
+            admins: vec![bob.clone()],
+            mutable: true,
+            owner: owner.clone(),
+        };
+        assert!(!config.can_modify(&alice));
+        assert!(config.can_modify(&bob));
+        assert!(config.can_modify(&owner));
+        // no one can modify an immutable contract
+        let config = AdminList {
+            admins: vec![alice.clone()],
+            mutable: false,
+            owner: owner.clone(),
+        };
+        assert!(!config.can_modify(&alice));
+        assert!(!config.can_modify(&bob));
+    }
+}
