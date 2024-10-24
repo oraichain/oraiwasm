60551e9a0988d942c58285f1049024e4a6812f9c
QuanPT <quanpt239@gmail.com>
2024-10-24 17:57:49 +0700

    feat(amm v3 rebalancer): add function create position

--- package/plus/amm-v3-rebalancer/src/contract.rs
+++ package/plus/amm-v3-rebalancer/src/contract.rs
@@ -1,14 +1,17 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::{
-    entry_point, to_json_binary, Addr, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo,
-    Response, StdResult, WasmMsg,
+    entry_point, to_json_binary, Addr, Api, Binary, Coin, CosmosMsg, Deps, DepsMut, Env,
+    MessageInfo, Response, StdResult, Uint128, WasmMsg,
 };
 use cw20::{BalanceResponse as Cw20BalanceResponse, Cw20QueryMsg::Balance as Cw20Balance};
 use oraiswap_v3_common::{
+    asset::AssetInfo,
+    math::{liquidity::Liquidity, sqrt_price::SqrtPrice},
     oraiswap_v3_msg::{ExecuteMsg as OraiswapV3ExecuteMsg, QueryMsg as OraiswapV3QueryMsg},
-    storage::Position,
+    storage::{PoolKey, Position},
 };
 
+use crate::asset::Asset;
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 use crate::state::{Config, CONFIG};
@@ -50,6 +53,28 @@ pub fn execute(
             wallet,
             amm_v3,
         } => update_config(deps, info, owner, executor, wallet, amm_v3),
+        ExecuteMsg::CreatePosition {
+            pool_key,
+            lower_tick,
+            upper_tick,
+            liquidity_delta,
+            slippage_limit_lower,
+            slippage_limit_upper,
+            amount_x,
+            amount_y,
+        } => create_position(
+            deps,
+            info,
+            env,
+            pool_key,
+            lower_tick,
+            upper_tick,
+            liquidity_delta,
+            slippage_limit_lower,
+            slippage_limit_upper,
+            amount_x,
+            amount_y,
+        ),
         ExecuteMsg::RemovePosition { index } => remove_position(deps, info, env, index),
         ExecuteMsg::SendToken { denom } => send_token(deps, info, env, denom),
     }
@@ -83,6 +108,88 @@ fn update_config(
     CONFIG.save(deps.storage, &config)?;
 
     Ok(Response::new().add_attributes(vec![("action", "update_config")]))
+}
+
+fn create_position(
+    deps: DepsMut,
+    info: MessageInfo,
+    env: Env,
+    pool_key: PoolKey,
+    lower_tick: i32,
+    upper_tick: i32,
+    liquidity_delta: Liquidity,
+    slippage_limit_lower: SqrtPrice,
+    slippage_limit_upper: SqrtPrice,
+    amount_x: u128,
+    amount_y: u128,
+) -> Result<Response, ContractError> {
+    let config = CONFIG.load(deps.storage)?;
+    if info.sender != config.executor {
+        return Err(ContractError::Unauthorized {});
+    }
+
+    let mut funds: Vec<Coin> = vec![];
+    let mut messages = vec![];
+
+    collect_and_increase_allowance(
+        deps.api,
+        &env,
+        &pool_key.token_x,
+        amount_x,
+        &mut funds,
+        &mut messages,
+        config.wallet.to_string(),
+        config.amm_v3.to_string(),
+    )?;
+    collect_and_increase_allowance(
+        deps.api,
+        &env,
+        &pool_key.token_y,
+        amount_y,
+        &mut funds,
+        &mut messages,
+        config.wallet.to_string(),
+        config.amm_v3.to_string(),
+    )?;
+
+    messages.push(CosmosMsg::Wasm(WasmMsg::Execute {
+        contract_addr: config.amm_v3.to_string(),
+        msg: to_json_binary(&OraiswapV3ExecuteMsg::CreatePosition {
+            pool_key,
+            lower_tick,
+            upper_tick,
+            liquidity_delta,
+            slippage_limit_lower,
+            slippage_limit_upper,
+        })?,
+        funds,
+    }));
+
+    Ok(Response::new()
+        .add_attributes(vec![("action", "create_position")])
+        .add_messages(messages))
+}
+
+fn collect_and_increase_allowance(
+    api: &dyn Api,
+    env: &Env,
+    denom: &String,
+    amount: u128,
+    coins: &mut Vec<Coin>,
+    msgs: &mut Vec<CosmosMsg>,
+    collect_from: String,
+    spender: String,
+) -> Result<(), ContractError> {
+    let asset = Asset::new(AssetInfo::from_denom(api, &denom), Uint128::from(amount));
+
+    // collect tokens
+    asset.transfer_from(env, msgs, collect_from, env.contract.address.to_string())?;
+
+    asset
+        .info
+        .increase_allowance(coins, msgs, spender, Uint128::from(amount))?;
+
+    Ok(())
 }
 
 fn remove_position(
